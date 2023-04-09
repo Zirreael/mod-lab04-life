e926237512f0d385764af63c76793840ff8ec6f8
Zirreael <126812503+Zirreael@users.noreply.github.com>
2023-04-09 12:56:39 +0300

    Create UnitTest1.cs

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using cli_life;
+
+namespase UnitTests
+{
+[TestClass]
+public class UnitTest1
+{
+[TestMethod]
+public void TestRead()
+{
+string name = "Block.txt";
+static Board board;
+Upload(name);
+Assert.IsTrue(board.Cells[0, 0].IsAlive);
+            Assert.IsTrue(board.Cells[0, 1].IsAlive);
+            Assert.IsTrue(board.Cells[1, 0].IsAlive);
+            Assert.IsTrue(board.Cells[1, 1].IsAlive);
+            }
+}
+}
